058fc91c8cc13da1fc7f646aa82c7d2897cd8be9
jer8ome <1762960779@qq.com>
2018-11-25 09:50:24 +0800

    2018-11-25

--- TableViewDemo/ViewController.swift
+++ TableViewDemo/ViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource{
+    var weapons = ["AUG","AKM","SCAR","M416","M16A4","AKM","SKS","UZI","AUG","AKM","SCAR","M416","M16A4","AKM","SKS","UZI"]
+    //数量
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weapons.count
+    }
+    //单元格
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeaponCell", for: indexPath)
+        cell.textLabel?.text = weapons[indexPath.row]
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
